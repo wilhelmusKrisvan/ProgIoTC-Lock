38d36f9e3a17775fabd36fd35679c9f37d21c011
Wilhelmus Krisvan <wilhelmus.krisvan@ti.ukdw.ac.id>
2019-11-10 19:43:54 +0700

    ANYAR sudah sama UI

---
+++ C-Lock/app/src/main/java/id/ac/ukdw/c_lock/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.ukdw.c_lock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
