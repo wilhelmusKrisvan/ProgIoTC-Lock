40f1fc64afaa6f4d5b1dd2001fc1be0326dec38e
Stefa <stefanus.adi@ti.ukdw.ac.id>
2019-11-12 14:19:15 +0700

    okok

--- C-Lock/app/src/main/java/id/ac/ukdw/c_lock/MainActivity.kt
+++ C-Lock/app/src/main/java/id/ac/ukdw/c_lock/MainActivity.kt
@@ -1,12 +1,32 @@
 package id.ac.ukdw.c_lock
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        btnLogin.setOnClickListener {
+            FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(txtEmail.text.toString(), txtPass.text.toString())
+                .addOnSuccessListener  {
+                    Toast.makeText(baseContext, "berhasil login", Toast.LENGTH_LONG).show()
+                    val i: Intent = Intent(baseContext, Home::class.java)
+                    startActivity(i)
+                }.addOnFailureListener {
+                    //FirebaseAuth.getInstance().createUserWithEmailAndPassword(edtEmail.text.toString(), edtPassword.text.toString())
+                    Toast.makeText(baseContext, "berhasil gagal login", Toast.LENGTH_LONG).show()
+                }
+
+        }
     }
+
+
 }
