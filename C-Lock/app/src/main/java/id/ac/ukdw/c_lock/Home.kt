41ec46779a06b776040eb03b1ea953d97cf4495c
Stefa <stefanus.adi@ti.ukdw.ac.id>
2019-11-21 18:02:16 +0700

    nih scan_yossie

--- C-Lock/app/src/main/java/id/ac/ukdw/c_lock/Home.kt
+++ C-Lock/app/src/main/java/id/ac/ukdw/c_lock/Home.kt
@@ -1,12 +1,45 @@
 package id.ac.ukdw.c_lock
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        btn_scan()
+    }
+
+    private fun btn_scan() {
+        btn_scan.setOnClickListener {
+            initScan()
+        }
+    }
+
+    private fun initScan() {
+        IntentIntegrator(this).initiateScan()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if(result.contents==null){
+                Toast.makeText(this, "the data is empty", Toast.LENGTH_LONG).show()
+            }else{
+                var text : String = result.contents.toString()
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+
+        }
+
     }
 }
