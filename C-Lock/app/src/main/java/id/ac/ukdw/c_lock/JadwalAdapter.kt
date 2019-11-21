378ac204c1d68ccbd2bb8d40b96a00cb16168379
Stefa <stefanus.adi@ti.ukdw.ac.id>
2019-11-21 18:12:54 +0700

    jadwal_adapter yossie

---
+++ C-Lock/app/src/main/java/id/ac/ukdw/c_lock/JadwalAdapter.kt
@@ -0,0 +1,23 @@
+package id.ac.ukdw.c_lock
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+
+class JadwalAdapter(val jadwal:ArrayList<String>) : RecyclerView.Adapter<JadwalAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JadwalAdapter.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: JadwalAdapter.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
