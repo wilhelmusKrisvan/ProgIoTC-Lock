ca4f64aeb0012ab99aaa661789d26585a8e41743
Stefa <stefanus.adi@ti.ukdw.ac.id>
2019-12-07 17:22:03 +0700

    logout gkbisa

--- C-Lock/app/src/main/java/id/ac/ukdw/c_lock/Home.kt
+++ C-Lock/app/src/main/java/id/ac/ukdw/c_lock/Home.kt
@@ -4,13 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.active_home.*
 
 class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.active_home)
+
+        logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            var i: Intent = Intent(this, LoginActivity::class.java)
+//        i.putExtra()
+            startActivity(i)
+        }
 
     }
 
